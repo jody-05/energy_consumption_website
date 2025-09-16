bc209f1802044ab9b049eb0e6471134880cc30d9
jody-05 <jodylaw0714@gmail.com>
2025-09-17 01:49:33 +0800

    website update

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+function moveToTelevision(){
+    window.location.href = 'television.html';
+}
+
+function moveToAboutUs(){
+    window.location.href = 'aboutus.html';
+}
+
+function moveToHome(){
+    window.location.href = 'home.html';
+}
